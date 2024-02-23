64009c0394786521483307db0bce88a191bba279
upadhyayvaibhav229 <upadhyayvaibhav229@gmail.com>
2024-02-24 00:30:52 +0530

    commited

--- SSP/Dashboard/forms.py
+++ SSP/Dashboard/forms.py
@@ -28,6 +28,6 @@ class DashboardForm(forms.Form):
 class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username','email', 'password1', 'password2']
+        fields = ['username', 'password1', 'password2']
 
         
